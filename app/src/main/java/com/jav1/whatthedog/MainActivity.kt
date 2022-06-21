e236ffcde985778b077286d41a3b59f309616cb1
Nomeos <mathieu.rabot01@gmail.com>
2022-06-21 12:33:09 +0200

    Add MainActivity Function with header comments

--- app/src/main/java/com/jav1/whatthedog/MainActivity.kt
+++ app/src/main/java/com/jav1/whatthedog/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.jav1.whatthedog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
+
+
+/**
+ * Main activity for the app.
+ * @author Mathieu RABOT
+ * @version 1.0
+ * This project is created for the purpose of learning Android development.
+ * By adding a map to an application by using the Osmdroid library
+ */
 class MainActivity : AppCompatActivity() {
+    /**
+     * Called when the activity is first created.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
