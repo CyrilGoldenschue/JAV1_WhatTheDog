0046672514bc0bc4956e85a173a75a2e97498ccb
Nomeos <mathieu.rabot01@gmail.com>
2022-06-21 13:31:52 +0200

    Create PolylineManager class the will manage the polyline addition, update etc..

---
+++ app/src/main/java/com/jav1/whatthedog/polyline/PolylineManager.kt
@@ -0,0 +1,13 @@
+package com.jav1.whatthedog.polyline
+
+/**
+ * Created by Mathieu RABOT
+ * 21.06.2022
+ * This class is used to create a line on the map
+ * using the GeoPoint class, the Polyline class and the MapView class from OSMdroid
+ * For the project what-the-dog JAV1
+ */
+class PolylineManager {
+    companion object {
+    }
+}
