df51b47125f3583255abd58c884bba766e2718f0
Nomeos <mathieu.rabot01@gmail.com>
2022-06-21 12:04:02 +0200

    Create a class that will manage the markers on the map

---
+++ app/src/main/java/com/jav1/whatthedog/MarkerManager.kt
@@ -0,0 +1,15 @@
+package com.jav1.whatthedog
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+
+/**
+ * Created by Mathieu RABOT
+ * 21.06.2022
+ * This class is used to create a marker on the map
+ * using the GeoPoint class, the Marker class and the MapView class from OSMdroid
+ * For the project what-the-dog JAV1
+ */
+class MarkerManager {
+
+}
