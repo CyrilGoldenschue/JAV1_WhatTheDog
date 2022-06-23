31a14168d750e29b90479a7eb13566f0e15502a6
Cyraullie <70378644+Cyraullie@users.noreply.github.com>
2022-06-23 08:59:24 +0200

    try work navigation

--- app/src/main/java/com/jav1/whatthedog/MainActivity.kt
+++ app/src/main/java/com/jav1/whatthedog/MainActivity.kt
@@ -2,10 +2,13 @@ package com.jav1.whatthedog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
     }
 }
