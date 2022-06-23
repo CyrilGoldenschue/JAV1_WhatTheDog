880f129c4f045fb113d96555d5d06fd71ba88ee8
CyrilGoldenschue <cyril.goldenschue@cpnv.ch>
2022-06-23 13:24:33 +0200

    Add data to use navigation

---
+++ app/src/main/java/com/example/drawerapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.drawerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close)
+        toggle.isDrawerIndicatorEnabled = true
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+
+
+
+
+
+
+
+    }
+}
